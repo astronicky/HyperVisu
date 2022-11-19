d91f4f30e2071afd0656a73c9c00c9bb513a0ef7
power <102508007+astronicky@users.noreply.github.com>
2022-11-19 08:19:19 -0500

    fix layout

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Layout from "../components/Layout/Layout";
 import { Text, Button } from "react-native";
 
 export default function LoginScreen({ navigation }) {
